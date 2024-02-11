c31d49e733e8fe717f5e4ae62df5df9d84d87719
Anastasiya Vasilevskaya <nastena2004vasil@gmail.com>
2024-02-11 23:58:14 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+//Напишите многопоточный ограниченный буфер с использованием ReentrantLock
+public class Main {
+    public static void main(String[] args) {
+        System.out.println();
+    }
+}
